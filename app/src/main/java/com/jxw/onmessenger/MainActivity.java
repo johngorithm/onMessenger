65a92806cf7c7440b4640b207f32c563870c1df5
John Obi <johngorithm@gmail.com>
2019-04-13 21:12:56 +0100

    feat(ui): add toolbar

--- app/src/main/java/com/jxw/onmessenger/MainActivity.java
+++ app/src/main/java/com/jxw/onmessenger/MainActivity.java
@@ -2,12 +2,19 @@ package com.jxw.onmessenger;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+    private Toolbar appToolbar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        appToolbar = findViewById(R.id.main_page_toolbar);
+        setSupportActionBar(appToolbar);
+        getSupportActionBar().setTitle("onMessenger");
     }
 }
