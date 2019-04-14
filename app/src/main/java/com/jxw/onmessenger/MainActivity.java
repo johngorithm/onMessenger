25c09270b8b48ecdb497dd1142cca63071126ec4
John Obi <johngorithm@gmail.com>
2019-04-14 11:50:35 +0100

    feat(ui): implement app tab layout
    - add custom tab layout
    - add view pager view for fragments
    - create a view pager adapter for main fragments
    
    [Delivers #002]

--- app/src/main/java/com/jxw/onmessenger/MainActivity.java
+++ app/src/main/java/com/jxw/onmessenger/MainActivity.java
@@ -1,11 +1,18 @@
 package com.jxw.onmessenger;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 
+import com.jxw.onmessenger.adapters.TabAccessAdapter;
+
 public class MainActivity extends AppCompatActivity {
     private Toolbar appToolbar;
+    private TabLayout mainTabLayout;
+    private ViewPager mainViewPager;
+    private TabAccessAdapter tabAccessAdapter;
 
 
     @Override
@@ -16,5 +23,14 @@ public class MainActivity extends AppCompatActivity {
         appToolbar = findViewById(R.id.main_page_toolbar);
         setSupportActionBar(appToolbar);
         getSupportActionBar().setTitle("onMessenger");
+
+        // View Pager
+        mainViewPager = findViewById(R.id.main_view_pager);
+        tabAccessAdapter = new TabAccessAdapter(getSupportFragmentManager());
+        mainViewPager.setAdapter(tabAccessAdapter);
+
+        // TabLayout
+        mainTabLayout = findViewById(R.id.main_tabs);
+        mainTabLayout.setupWithViewPager(mainViewPager);
     }
 }
---
+++ app/src/main/java/com/jxw/onmessenger/adapters/TabAccessAdapter.java
@@ -0,0 +1,51 @@
+package com.jxw.onmessenger.adapters;
+
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import com.jxw.onmessenger.views.ChatsFragment;
+import com.jxw.onmessenger.views.ContactsFragment;
+import com.jxw.onmessenger.views.GroupsFragment;
+
+public class TabAccessAdapter extends FragmentPagerAdapter {
+    public TabAccessAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                return new ChatsFragment();
+            case 1:
+                return new GroupsFragment();
+            case 2:
+                return new ContactsFragment();
+            default:
+                return null;
+        }
+
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position) {
+            case 0:
+                return "Chats";
+            case 1:
+                return "Groups";
+            case 2:
+                return "Contacts";
+            default:
+                return null;
+        }
+    }
+}
