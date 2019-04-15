6d5bfc0854eef2dce4f90fcf3025fc6c930528c8
John Obi <johngorithm@gmail.com>
2019-04-15 16:46:27 +0100

    feat(auth): implement logout functionality
    - add menu options
    - logout user on logout option click
    - redirect user to login activity on logout
    
    [Delivers #005]

--- app/src/main/java/com/jxw/onmessenger/MainActivity.java
+++ app/src/main/java/com/jxw/onmessenger/MainActivity.java
@@ -7,6 +7,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -20,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
     private TabAccessAdapter tabAccessAdapter;
 
     private FirebaseUser currentUser;
+    private FirebaseAuth firebaseAuth;
     private static final String TAG = "AUTH";
 
 
@@ -27,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        firebaseAuth = FirebaseAuth.getInstance();
         currentUser = firebaseAuth.getCurrentUser();
 
         appToolbar = findViewById(R.id.main_page_toolbar);
@@ -57,5 +61,31 @@ public class MainActivity extends AppCompatActivity {
     private void sendUserToLoginActivity() {
         Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
         startActivity(loginIntent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+        switch (item.getItemId()) {
+            case R.id.option_find_friends:
+                Toast.makeText(this, "Find Friends", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.option_settings:
+                Toast.makeText(this, "Settings", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.option_log_out:
+                firebaseAuth.signOut();
+                sendUserToLoginActivity();
+                return true;
+            default:
+                return false;
+        }
     }
 }
